2daf2847fbd33a1bd9402b4ea1812d257958cd72
sozibhossain <sozibhossain69@gmail.com>
2024-02-19 12:06:30 +0600

    first commit

---
+++ multiple-condition.js
@@ -0,0 +1,24 @@
+var gotJob = true;
+var monySave = 100000;
+var hasFlat = true;
+var hasHouse = true;
+
+// if(gotJob == true && monySave > 200000 && hasFlat == true){
+//     console.log("cholo biya kore feli!!")
+// }
+// else{
+//     console.log("Tor kopale biya nai!!")
+// }
+
+// if(gotJob == true || monySave > 200000 || hasFlat == true){
+//     console.log("cholo biya kore feli!!")
+// }
+// else{
+//     console.log("Tor kopale biya nai!!")
+// }
+
+if ((gotJob == true && monySave > 200000) || hasHouse == true) {
+  console.log("cholo biya kore feli!!");
+} else {
+  console.log("Tor kopale biya nai!!");
+}
---
+++ nested-conditions.js
@@ -0,0 +1,31 @@
+var danishPrice = 50;
+var packedwell = true;
+var butterBreadprice = 25;
+var tostBiscuitPrice = 10;
+var mybudget = 7;
+
+// if (danishPrice < mybudget) {
+//   console.log("Danish kyaye danish jabo!!");
+// } else if (butterBreadprice < mybudget) {
+//   console.log("butter bon diye cha khamu");
+// } else if (tostBiscuitPrice < mybudget) {
+//   console.log("Tost biscuit diya cha khamu");
+// } else {
+//   console.log("Batasha diya cha khamu");
+// }
+
+
+if (danishPrice < mybudget) {
+    if(packedwell = true){
+        console.log("denish ami khabo na")
+    }else{
+        console.log("Danish kyaye danish jabo!!");
+    }
+  
+} else if (butterBreadprice < mybudget) {
+  console.log("butter bon diye cha khamu");
+} else if (tostBiscuitPrice < mybudget) {
+  console.log("Tost biscuit diya cha khamu");
+} else {
+  console.log("Batasha diya cha khamu");
+}
